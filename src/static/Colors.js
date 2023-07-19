b7ff8d722ef2ab3bcfb1cdefce90a15c98fab550
JereUR <jeremias.jdv@gmail.com>
2023-07-19 03:47:17 -0300

    feat(Root): Constants colors

---
+++ src/static/Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  backgroundColor:"#E8F1F2",
+  headerColor: '#384C5A',
+  primaryText: '#333333',
+  veryColdTemperature: '#2B6CC4',
+  coldTemperature: '#5DADEC',
+  normalTemperature: '#F7B165',
+  hotTemperature: '#F7956B',
+  veryHotTemperature: '#FF4444'
+};
