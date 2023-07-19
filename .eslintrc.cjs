4939035e5551a991e4a852f2ddcb8499564bcaa6
JereUR <jeremias.jdv@gmail.com>
2023-07-19 03:45:32 -0300

    fix(Root): Prettier fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,14 +7,15 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh, "prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 }
