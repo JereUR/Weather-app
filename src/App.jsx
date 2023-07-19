2c89744b972603bcbfab69afcd20ebff27bd4238
JereUR <jeremias.jdv@gmail.com>
2023-07-19 03:46:11 -0300

    feat(Root): Component called

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import { useEffect, useState } from 'react'
-import GetUserCity from './helpers/GetUserCity'
-import fetchWeatherData from './helpers/GetWeatherData'
+
+import { Colors } from './static/Colors';
+
+import WeatherData from './components/WeatherData'
 
 function App() {
-  const [city, setCity] = useState('')
-  const [weatherData, setWeatherData] = useState(null)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const userCity = await GetUserCity()
-        console.log(userCity)
-        setCity(userCity.city)
-
-        const weather = await fetchWeatherData({
-          latitude: userCity.userLocation.latitude,
-          longitude: userCity.userLocation.longitude
-        })
-        console.log(weather)
-        setWeatherData(weather)
-      } catch (error) {
-        console.log('Error al obtener los datos del clima:', error)
-      }
-    }
-
-    fetchData()
-  }, [])
-
   return (
     <div>
-      {city ? (
-        <p>La ciudad del usuario es: {city}</p>
-      ) : (
-        <p>Cargando la ubicación del usuario...</p>
-      )}
-
-      {weatherData && (
-        <div>
-          <h2>Datos del clima:</h2>
-          <p>Temperatura: {weatherData.temperature}°C</p>
-          <p>Condición: {weatherData.condition}</p>
-        </div>
-      )}
+      <WeatherData />
     </div>
   )
 }
